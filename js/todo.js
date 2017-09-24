bccbcf6848bdc1c1c1e5ff04bab0e1d94bf24da6
sophiehelf <sophie.helf@gmail.com>
2017-09-24 17:11:15 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm();
 });
 
 // define functions here
+
+function submitForm() {
+	$('form').on('submit', function(e){
+		var listItem = $('#item').val();
+		$('ol').append('<li>' + listItem + '</li>');
+		e.preventDefault();
+	})
+}
+
